19549169f65a89eb2352975c82fadea5a084b1ec
dotoan04 <dotoan159@gmail.com>
2025-10-20 23:15:28 +0700

    FIx

--- src/app/api/work-experience/[id]/route.ts
+++ src/app/api/work-experience/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 
-import { authOptions } from '@/lib/auth/options'
+import { authOptions } from '@/lib/auth/config'
 import { workExperienceSchema } from '@/lib/validators/work-experience'
 import {
   deleteWorkExperience,
